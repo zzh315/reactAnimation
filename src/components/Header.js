a2fc183bae558a767b93b4b0344314fe7523675a
zzh315 <zhengzh315@gmail.com>
2023-05-17 18:59:46 +1000

    animation for header and base

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,17 @@ const Header = () => {
           <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
         </svg>
       </div>
-      <motion.div initial={{ y: -250 }} animate={{ y: -10 }} className="title">
+      <motion.div
+        initial={{ y: -250 }}
+        animate={{ y: -10 }}
+        transition={{
+          delay: 0.1,
+          type: "spring",
+
+          stiffness: 50,
+        }}
+        className="title"
+      >
         <h1>Pizza Joint</h1>
       </motion.div>
     </header>
